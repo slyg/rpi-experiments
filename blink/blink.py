a93b934d73b4ab72d4680acce0e4a296262997bb
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-05-23 23:05:13 +0100

    Add containerized blink

---
+++ blink/blink.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+# Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
+GPIO.setmode(GPIO.BCM)
+
+red_pin = 18
+
+GPIO.setup(red_pin, GPIO.OUT)
+
+
+try:
+    while True:
+        GPIO.output(red_pin, True)  # LED on
+        time.sleep(0.5)             # delay 0.5 seconds
+        GPIO.output(red_pin, False) # LED off
+        time.sleep(0.5)             # delay 0.5 seconds
+finally:
+    print("Cleaning up")
+    GPIO.cleanup()
