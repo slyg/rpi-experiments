395056d758a94d1306acaee6d0135c3007716586
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-06-04 03:07:21 +0100

    Add dockerized node version of blink

---
+++ blink-node/index.js
@@ -0,0 +1,8 @@
+const five = require('johnny-five')
+const Raspi = require('raspi-io')
+const board = new five.Board({ io: new Raspi() })
+
+board.on('ready', function() {
+  const led = new five.Led('P1-12')
+  led.blink()
+})
