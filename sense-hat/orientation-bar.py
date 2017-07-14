33607b66b571dc47d67eeb2a7994c1e470d52262
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-07-15 00:03:35 +0100

    Fix row number

--- sense-hat/orientation-bar.py
+++ sense-hat/orientation-bar.py
@@ -3,6 +3,7 @@ import math
 
 sense = SenseHat()
 sense.set_rotation(90)
+rows = 8
 
 colours_per_row = [
   (242,6,104),
@@ -14,7 +15,6 @@ colours_per_row = [
   (252,94,48),
   (242,6,104)
 ]
-
 black = (0, 0, 0)
 
 # Shift-lefting orientation angle
@@ -22,13 +22,13 @@ def shiftAngle (angle):
   return (angle - 180) if angle > 180 else (angle + 180)
 
 def pixelRow(angle):
-  normalized_angle = math.floor( 8 * shiftAngle(angle) / 360 )
-  return [colours_per_row[i] if i == normalized_angle else black for i in range(8)]
+  normalized_angle = math.floor( rows * shiftAngle(angle) / 360 )
+  return [colours_per_row[i] if i == normalized_angle else black for i in range(rows)]
 
 while True:
     roll = sense.get_orientation_degrees()['roll']
 
     pixels = []
-    for num in range(8): pixels = pixels + pixelRow(roll)
+    for num in range(rows): pixels = pixels + pixelRow(roll)
 
     sense.set_pixels(pixels)
