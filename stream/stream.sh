638bfb7ed9de0c1245acb085192f251787f5237e
Sylvain Faucherand <syl.faucherand@gmail.com>
2017-09-17 22:11:17 +0100

    Add reminder

--- stream/stream.sh
+++ stream/stream.sh
@@ -5,3 +5,5 @@ raspivid -o - \
   -w 640 \
   -h 480 \
   -fps 20 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
+
+# then open http://<rpi>:8160 in VLC
